dbb801fbc3601923b6475eb4cf4606480f4d8979
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2025-02-28 10:37:35 -0600

    fix submission

--- docker/orthanc/StoragePlugin.cpp
+++ docker/orthanc/StoragePlugin.cpp
@@ -113,8 +113,8 @@ static OrthancPluginErrorCode StorageCreate(const char* uuid,
     else
     {
       OrthancPlugins::LogInfo("Writing non-encrypted file");
-      OrthancPlugins::LogInfo("size =" + size);
-      OrthancPlugins::LogInfo("content =" + content);
+      OrthancPlugins::LogInfo("size =" + (size ? size : "null"));
+      OrthancPlugins::LogInfo("content =" + (content ? content : "null"));
       writer->Write(reinterpret_cast<const char*>(content), size);
       OrthancPlugins::LogInfo("Done writing");
     }
