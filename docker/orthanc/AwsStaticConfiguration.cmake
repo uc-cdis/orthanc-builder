43fdbdd601b45bbd361c1862f3c82a0c38c1177d
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2025-03-21 16:24:34 -0500

    add debug logs

--- docker/orthanc/AwsStaticConfiguration.cmake
+++ docker/orthanc/AwsStaticConfiguration.cmake
@@ -34,8 +34,17 @@ SET(AWS_SDK_CPP_VERSION "1.11.520")
 SET(AWS_C_COMMON_SOURCES_DIR ${CMAKE_BINARY_DIR}/aws-c-common-${AWS_C_COMMON_VERSION})  # source =  https://github.com/awslabs/aws-c-common/archive/refs/tags/v0.9.3.tar.gz
 SET(AWS_C_COMMON_URL "https://github.com/awslabs/aws-c-common/archive/refs/tags/v${AWS_C_COMMON_VERSION}.tar.gz")
 SET(AWS_C_COMMON_MD5 "no-check")
+message("About to run DownloadPackage")
 DownloadPackage(${AWS_C_COMMON_MD5} ${AWS_C_COMMON_URL} "${AWS_C_COMMON_SOURCES_DIR}")
+
+execute_process(COMMAND ls ${AWS_C_COMMON_SOURCES_DIR}/source/external/libcbor/cbor OUTPUT_VARIABLE LS_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
+message(FATAL_ERROR "List: ${LS_OUTPUT}")
+
+message("About to run python")
 execute_process(COMMAND python ${AWS_C_COMMON_SOURCES_DIR}/scripts/import_libcbor.py)
+
+execute_process(COMMAND ls ${AWS_C_COMMON_SOURCES_DIR}/source/external/libcbor/cbor OUTPUT_VARIABLE LS_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
+message(FATAL_ERROR "List: ${LS_OUTPUT}")
 
 SET(AWS_CHECKSUMS_SOURCES_DIR ${CMAKE_BINARY_DIR}/aws-checksums-${AWS_CHECKSUMS_VERSION})  # source =  https://github.com/awslabs/aws-checksums/archive/refs/tags/v0.1.17.tar.gz
 SET(AWS_CHECKSUMS_URL "https://github.com/awslabs/aws-checksums/archive/refs/tags/v${AWS_CHECKSUMS_VERSION}.tar.gz")
